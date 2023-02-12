5730aa13f408a4953177532654e6215338410572
沐笙 <3560775787@qq.com>
2023-02-12 16:46:04 +0800

    调试文件

---
+++ JavaProject/T230212/src/T1.java
@@ -0,0 +1,16 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * 创建于 IntelliJ IDEA.
+ * 描述：
+ * User: DR
+ * Date: 2023-2-12
+ * Time: 16:30
+ */
+public class T1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试!");
+        System.out.println("测试2");
+    }
+
+}
