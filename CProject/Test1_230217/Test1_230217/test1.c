f072ebf7b7b81a8f4f824596caf251128d38b6cd
沐笙 <3560775787@qq.com>
2023-02-27 22:21:31 +0800

    更新230227

--- CProject/Test1_230217/Test1_230217/test1.c
+++ CProject/Test1_230217/Test1_230217/test1.c
@@ -4,45 +4,95 @@
  »úÆ÷£ºDR
  ±àÒëÆ÷£ºVS2019
 */
-#include <stdio.h>
-#include <string.h>
-void remove_common(char* delpos, int longth, int sub)
-{
-	for (int i = 0; i < longth - sub; i++)
-	{
-		delpos[i] = delpos[i + 1];
-	}
-}
+//#include <stdio.h>
+//#include <string.h>
+//void remove_common(char* delpos, int longth, int sub)
+//{
+//	for (int i = 0; i < longth - sub; i++)
+//	{
+//		delpos[i] = delpos[i + 1];
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[100] = { 0 };
+//
+//	char arr2[100] = { 0 };
+//	char* pos2 = arr2;
+//
+//	gets(arr1);
+//	gets(arr2);
+//
+//	int longth = strlen(arr1);
+//
+//	while ((*pos2) != '\0')
+//	{
+//		char* pos1 = arr1;
+//		int sub = 0;
+//		while ((*pos1) != '\0')
+//		{
+//			if (*pos2 == *pos1)
+//			{
+//				remove_common(pos1, longth, sub);
+//				continue;
+//			}
+//			pos1++;
+//			sub++;
+//		}
+//		pos2++;
+//	}
+//
+//	printf("%s", arr1);
+//	return 0;
+//}
+//#include <stdio.h>
+//#define N 4
+//
+//#define Y(n) ((N+2)*n)
+//int main()
+//{
+//	int z = 2 * (N + Y(5 + 1));
+//	printf("%d", z);
+//	return 0;
+//}
 
-int main()
-{
-	char arr1[100] = { 0 };
 
-	char arr2[100] = { 0 };
-	char* pos2 = arr2;
 
-	gets(arr1);
-	gets(arr2);
+//#include <stdio.h>
+//typedef struct MyStruct
+//{
+//	int a;
+//	char b;
+//	int c;
+//}MyStruct;
+//
+//#define OFFSETOF(Type,Mumber) (int)( &( ( (Type*)0 )->Mumber ) )
 
-	int longth = strlen(arr1);
+//int main()
+//{
+//	//int offset = OFFSETOF(MyStruct, c);
+//	//printf("%d", offset);
+//	int* p = *(int*)0;
+//	return 0;
+//}
 
-	while ((*pos2) != '\0')
-	{
-		char* pos1 = arr1;
-		int sub = 0;
-		while ((*pos1) != '\0')
-		{
-			if (*pos2 == *pos1)
-			{
-				remove_common(pos1, longth, sub);
-				continue;
-			}
-			pos1++;
-			sub++;
-		}
-		pos2++;
-	}
+//#define A 100;
 
-	printf("%s", arr1);
-	return 0;
-}
+//#define A 100
+
+//#define B (5+5)
+
+//#include <stdio.h>
+//
+//#define EPB(x) ((x&0xAAAAAAAA)>>1)+((x&(0xAAAAAAAA>>1))>>1)
+//
+//int main()
+//{
+//	int a = 170;
+//	//0000 0000 0000 0000 0000 0000 1010 1010
+//	int b = EPB(a);
+//	//ÆæÅ¼bitÎ»½»»»ºó
+//	//0000 0000 0000 0000 0000 0000 0101 0101
+//	return 0;
+//}
---
+++ JavaProject/T230227/src/TestDemo1.java
@@ -0,0 +1,11 @@
+/**
+ * 创建于 IntelliJ IDEA.
+ * 描述：
+ * User: DR
+ * Date: 2023/2/27
+ * Time: 20:08
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+    }
+}
