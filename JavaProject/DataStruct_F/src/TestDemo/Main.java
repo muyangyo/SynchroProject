8f73e7565f0e3802c508c216f4520803e7b42770
沐阳 <3560775787@qq.com>
2023-04-04 23:44:35 +0800

    更新230404

---
+++ JavaProject/DataStruct_F/src/TestDemo/Main.java
@@ -0,0 +1,61 @@
+package TestDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建于 IntelliJ IDEA.
+ * 描述：
+ * User: DR
+ * Date: 2023/4/4
+ * Time: 19:41
+ */
+public class Main {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        ret.add(list);
+        for (int i = 1; i < numRows; i++) {
+
+            List<Integer> curRow = new ArrayList<>();
+            curRow.add(1);
+            //处理中间的数字
+            for (int j = 1; j < i; j++) {
+                curRow.add(ret.get(i - 1).get(j) + ret.get(i - 1).get(j - 1));
+            }
+
+            //最后一个数字1
+            curRow.add(1);
+
+            ret.add(curRow);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        List<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("abc");
+        arrayList1.add("def");
+        arrayList1.add("abc");
+        System.out.println("原本的数组: " + arrayList1);
+
+        List<String> arrayList2 = arrayList1.subList(0, 2);
+        System.out.println("截取部分: " + arrayList2);
+
+        arrayList2.add("def");
+        System.out.println("原本的数组: " + arrayList1);
+        System.out.println("截取部分: " + arrayList2);
+    }
+
+    /*public static String func(String str1, String str2) {
+        String Temp = str1;
+        for (int i = 0; i < str2.length(); i++) {
+            String temp = String.valueOf(str2.charAt(i));
+            Temp = Temp.replaceAll(temp, "");
+        }
+        return Temp;
+    }*/
+}
