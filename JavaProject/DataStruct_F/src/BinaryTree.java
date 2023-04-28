6bed613f797ff6f1d7a148b18d7a187e6028f431
沐阳 <3560775787@qq.com>
2023-04-28 22:50:42 +0800

    更新 BinaryTree.java

--- JavaProject/DataStruct_F/src/BinaryTree.java
+++ JavaProject/DataStruct_F/src/BinaryTree.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class BinaryTree {
     static class BinaryNode {
-        public int val;
+        public char val;
         public BinaryNode left;
         public BinaryNode right;
 
@@ -21,28 +21,26 @@ public class BinaryTree {
 
     //前序打印:根 -> 左 -> 右
     public static void preOrder(BinaryNode root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         System.out.println(root.val);
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    public static List<Integer> ret = new ArrayList<>();
+    /*public static List<Integer> ret = new ArrayList<>();
+
     public static List<Integer> preOrder1(BinaryNode root) {
-        if (root == null)
-            return null;
+        if (root == null) return null;
         ret.add(root.val);
         preOrder1(root.left);
         preOrder1(root.right);
         return ret;
-    }
+    }*/
 
 
     //中序打印:左 -> 根 -> 右
     public static void inOrder(BinaryNode root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         inOrder(root.left);
         System.out.println(root.val);
         inOrder(root.right);
@@ -50,8 +48,7 @@ public class BinaryTree {
 
     //后序打印:左 -> 右 -> 根
     public static void postOrder(BinaryNode root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         postOrder(root.left);
         postOrder(root.right);
         System.out.println(root.val);
@@ -90,19 +87,18 @@ public class BinaryTree {
     }
 
     public static int Size(BinaryNode root) {
-        if (root == null)
-            return 0;
+        if (root == null) return 0;
         return 1 + Size(root.right) + Size(root.left);
     }
 
     //求叶子结点数
     public static int CountForLeaf = 0;
+
     public static int LeafNodeCount(BinaryNode root) {
         if (root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null)
-            CountForLeaf++;
+        if (root.left == null && root.right == null) CountForLeaf++;
         LeafNodeCount(root.right);
         LeafNodeCount(root.left);
         return CountForLeaf;
@@ -113,10 +109,94 @@ public class BinaryTree {
             return 0;
         }
         int count = 0;
-        if (root.left == null && root.right == null)
-            return 1;
+        if (root.left == null && root.right == null) return 1;
         count += LeafNodeCountRet(root.right);
         count += LeafNodeCountRet(root.left);
         return count;
+    }
+
+    /*
+    新
+     */
+
+    //获取第 K 层的结点个数
+    public static int getKLevelNodeCount(BinaryNode root, int k) {
+        int ret = 0;
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        ret += getKLevelNodeCount(root.left, k - 1);
+        ret += getKLevelNodeCount(root.right, k - 1);
+        return ret;
+    }
+
+    //获取树的高度
+    public static int getHeight(BinaryNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int LHeight = getHeight(root.left);
+        int RHeight = getHeight(root.right);
+        return 1 + (LHeight > RHeight ? LHeight : RHeight);
+    }
+
+    //寻值(前序遍历法)
+    public static BinaryNode FindValue_pro(BinaryNode root, char key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            return root;
+        }
+        BinaryNode retL = FindValue_pro(root.left, key);
+        BinaryNode retR = FindValue_pro(root.right, key);
+        return retL == null ? retR : retL;
+    }
+
+    public boolean isSameTree(BinaryNode p, BinaryNode q) {
+        //先对根判断避免为空的情况
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null || p != null && q == null) {
+            return false;
+        }
+
+        //对根判值
+        if (p.val != q.val) {
+            return false;
+        }
+
+        //再对左右树进行判断
+        boolean flagL = false;
+        flagL = isSameTree(p.left, q.left);
+        if (flagL == false)
+            return false;
+
+        boolean flagR = false;
+        flagR = isSameTree(p.right, q.right);
+        if (flagR == false)
+            return false;
+
+        return true;
+    }
+
+    public boolean isSubtree(BinaryNode root, BinaryNode subRoot) {
+        if (root == null)//空树是不可能为任何树的父树的
+            return false;
+        if (subRoot == null)//空树是任何树的子树
+            return true;
+        if (isSameTree(root, subRoot))
+            return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    //这是一个 ,使用特殊情况帮我测试一下,看看是否会出错
+    public static void main(String[] args) {
+        BinaryNode root = CreateTree();
+        System.out.println(FindValue_pro(root, 'H').val);
     }
 }
