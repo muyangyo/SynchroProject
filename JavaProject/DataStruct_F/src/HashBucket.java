440cda614fb0d90b8eac1a2004399648a7e9fd9e
沐阳 <3560775787@qq.com>
2023-06-13 00:46:09 +0800

    更新230613

--- JavaProject/DataStruct_F/src/HashBucket.java
+++ JavaProject/DataStruct_F/src/HashBucket.java
@@ -10,7 +10,7 @@ public class HashBucket {
 
     //数组挂载结点
     static class Node {
-        private int key;
+        private Integer key;
         private int value;
         private Node next;
 
@@ -20,27 +20,68 @@ public class HashBucket {
         }
     }
 
-    public int usedSize; //元素个数
+    public int usedSize; //元素个数(不是链表头的个数,是全部元素)
     private static final float DEFAULT_LOAD_FACTOR = 0.75f; //默认的负载因子为0.75
 
     public HashBucket() {
-        this.array = new Node[10];
+        this.array = new Node[10];//初始化为 10 个桶
     }
 
     public void put(int key, int val) {
-        // code here
+        Node tempNode = new Node(key, val);
+        int hash = tempNode.key.hashCode();//生成 hash值
+        int index = getIndex(hash);//获取下标
+
+        //开始寻找
+        Node cur = array[index];
+        while (cur != null) {
+            if (cur.key.equals(tempNode.key)) {
+                System.out.println("已存在该元素,仅更新 val 值");
+                cur.value = val;
+                return;//更新完退出即可
+            }
+            cur = cur.next;//向后走
+        }
+
+        // cur 为 null 了,即没有该元素
+        //采用链表头插法(更简单点)
+        tempNode.next = array[index];
+        array[index] = tempNode;
+        usedSize++;
+
+        //判断负载因子是否超过 0.75 ,超过则扩容,但是扩容后会导致 Index 改变,则全部需要重新哈希后重新放置
+        if (loadFactor() >= DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+    }
+
+
+    private int getIndex(int hash) {
+        return hash % array.length;
     }
 
     private void resize() {
-        // code here
+        //todo
     }
 
     private float loadFactor() {
-        // code here
+        return ((float) usedSize) / array.length; // 负载因子 = 填入表中的元素个数 / 数组长度
     }
 
-    public int get(int key) {
-        // code here
+    public int getVal(Integer key) {
+        int hash = key.hashCode();
+        int index = getIndex(hash);
+
+        Node cur = array[index];
+        while (cur != null) {
+            if (cur.key.equals(key)) {
+                return cur.value;//找到回返 val
+            }
+            cur = cur.next;
+        }
+
+        //没找到
+        return -1;
     }
-}
+
 }
