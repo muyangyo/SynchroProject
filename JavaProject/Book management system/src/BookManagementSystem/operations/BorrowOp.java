4f0381e5dbda7535cf483ef49da27e5120e3c02a
沐阳 <3560775787@qq.com>
2023-07-03 11:36:44 +0800

    更新230703
    修:
    1.图书管理系统的借书空指针问题

--- JavaProject/Book management system/src/BookManagementSystem/operations/BorrowOp.java
+++ JavaProject/Book management system/src/BookManagementSystem/operations/BorrowOp.java
@@ -18,13 +18,13 @@ public class BorrowOp implements OPeration {
         Scanner scanner = new Scanner(System.in);
         System.out.print("请输入需要借出的书名: ");
         String BookName = scanner.nextLine();
-        for (int i = 0; i < bookList.Books.length; i++) {
-            if (bookList.Books[i].getBookName().equals(BookName) && bookList.Books[i].getBorrowed()) {
+        for (int i = 0; i < bookList.UsedSize; i++) {
+            if (bookList.Books[i].getBookName().equals(BookName) && !bookList.Books[i].getBorrowed()) {
                 System.out.println("成功借阅:" + bookList.Books[i]);
                 bookList.Books[i].setBorrowed(true);
                 return;
             }
         }
-        System.out.println("未找到该图书");
+        System.out.println("未找到该图书/已经被借出");
     }
 }
