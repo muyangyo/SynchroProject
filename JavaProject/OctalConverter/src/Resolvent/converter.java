4f137641b46afe89b5d36a8f3e327dadf666e000
沐阳 <3560775787@qq.com>
2023-07-29 20:06:20 +0800

    更新230729
    更新阻塞队列的实现

--- JavaProject/OctalConverter/src/Resolvent/converter.java
+++ JavaProject/OctalConverter/src/Resolvent/converter.java
@@ -1,5 +1,7 @@
 package Resolvent;
 
+import com.sun.corba.se.impl.oa.toa.TOA;
+
 import java.util.*;
 
 import static java.util.Spliterators.iterator;
@@ -12,21 +14,31 @@ import static java.util.Spliterators.iterator;
  * Time:20:43
  */
 public class converter {
-    private final static int TARGET_BASE = 2;
+    private static int TARGET_BASE = 2;//目标进制 <= 10
 
-    public static String To(int Ten) {
+    public static String To(int Ten, int target) {
+        if (target > 10) throw new RuntimeException("只能输入 2/8/10 !");
+        TARGET_BASE = target;
         List<Integer> list = new LinkedList<>();
+
+        boolean isMinus = false;
+        //负数先取正
+        if (Ten < 0) {
+            isMinus = true;
+            Ten = Math.abs(Ten);
+        }
         while (Ten != 0) {
             int num = Ten % TARGET_BASE;
             Ten /= TARGET_BASE;
             list.add(num);
         }
-        if (list.isEmpty())
-            throw new RuntimeException("数值错误!");
+        if (list.isEmpty()) throw new RuntimeException("数值错误!");
         Collections.reverse(list);
 
         ListIterator<Integer> iterator = list.listIterator(0);
         StringBuilder stringBuilder = new StringBuilder();
+        //负数追加一个 负号
+        if (isMinus) stringBuilder.append("-");
         int count = 0;
         while (iterator.hasNext()) {
             stringBuilder.append("" + iterator.next());
---
+++ JavaProject/SystemCode/src/myBlockingQueue.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建于IntelliJ IDEA.
+ * 描述:
+ * User:MuYang
+ * Date 2023/7/29
+ * Time:17:55
+ */
+public class myBlockingQueue<T> {
+    private T[] queue = (T[]) new Object[100];//循环队列
+    private volatile int head = 0;//头
+    private volatile int tail = 0;//尾
+    private volatile int usedSize = 0;//元素个数
+
+    //入队列
+    public void put(T x) throws InterruptedException {
+
+        // 推荐写成这样而不是加到函数名上,这样后面更容易知道锁是哪个对象
+        synchronized (this) {
+            while (usedSize == queue.length) {
+                //队列满,阻塞处于 put 的进程
+                this.wait();
+            }
+            queue[tail] = x;
+            tail = (tail + 1) % queue.length;
+            usedSize++;
+            //唤醒处于 take 的线程,如果有阻塞就唤醒,没有则无视
+            this.notify();
+        }
+
+    }
+
+    //出队列
+    public T take() throws InterruptedException {
+        synchronized (this) {
+            while (usedSize == 0) {
+                //队列为空的情况,阻塞处于 take 的进程
+                this.wait();
+            }
+            T ret = queue[head];
+            head = (head + 1) % queue.length;
+            usedSize--;
+            this.notify();
+            return ret;
+        }
+    }
+}
