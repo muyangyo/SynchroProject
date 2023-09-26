a2626f60c39298bb2aa0a8352a1a65fc202da8fc
沐阳 <3560775787@qq.com>
2023-09-26 22:50:39 +0800

    Update Demo230923.java

--- JavaProject/WrittenExamTraining/src/Demo230923.java
+++ JavaProject/WrittenExamTraining/src/Demo230923.java
@@ -10,6 +10,159 @@ import java.util.*;
  */
 public class Demo230923 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            long M = in.nextLong();
+            int N = in.nextInt();
+
+            if (M > 0) {
+                if (N < 10) {
+                    List<Integer> list = new LinkedList<>();
+                    while (M != 0) {
+                        int temp = (int) M % N;
+                        list.add(temp);
+                        M /= N;
+                    }
+                    Collections.reverse(list);
+                    for (int temp : list) {
+                        System.out.print(temp);
+                    }
+                    System.out.print("\n");
+                } else if (N == 10) {
+                    System.out.println(M);
+                } else {
+                    //大于10的情况
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while (M != 0) {
+                        int temp = (int) M % N;
+                        if (temp >= 10) {
+                            switch (temp) {
+                                case 10:
+                                    stringBuilder.append("A");
+                                    break;
+                                case 11:
+                                    stringBuilder.append("B");
+                                    break;
+                                case 12:
+                                    stringBuilder.append("C");
+                                    break;
+                                case 13:
+                                    stringBuilder.append("D");
+                                    break;
+                                case 14:
+                                    stringBuilder.append("E");
+                                    break;
+                                case 15:
+                                    stringBuilder.append("F");
+                                    break;
+                            }
+                        } else
+                            stringBuilder.append(temp);
+                        M /= N;
+                    }
+                    stringBuilder.reverse();
+                    System.out.println(stringBuilder);
+                }
+            } else {
+                M = Math.abs(M);
+                if (N == 2) {
+                    List<Integer> list = new LinkedList<>();
+                    while (M != 0) {
+                        int temp = (int) M % N;
+                        list.add(temp);
+                        M /= N;
+                    }
+                    Collections.reverse(list);
+                    int count = 32 - list.size();
+                    boolean first = true;
+                    for (; count > 0; count--) {
+                        if (first) {
+                            System.out.print("1");
+                            first = false;
+                        } else System.out.print("0");
+                    }
+                    for (int temp : list) {
+                        System.out.print(temp);
+                    }
+                    System.out.print("\n");
+
+                } else if (N < 10) {
+                    List<Integer> list = new LinkedList<>();
+                    while (M != 0) {
+                        int temp = (int) M % N;
+                        list.add(temp);
+                        M /= N;
+                    }
+                    Collections.reverse(list);
+                    System.out.print("-");
+                    for (int temp : list) {
+                        System.out.print(temp);
+                    }
+                    System.out.print("\n");
+                } else if (N == 10) {
+                    System.out.print("-");
+                    System.out.println(M);
+                } else {
+                    //大于10的情况
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while (M != 0) {
+                        int temp = (int) M % N;
+                        if (temp >= 10) {
+                            switch (temp) {
+                                case 10:
+                                    stringBuilder.append("A");
+                                    break;
+                                case 11:
+                                    stringBuilder.append("B");
+                                    break;
+                                case 12:
+                                    stringBuilder.append("C");
+                                    break;
+                                case 13:
+                                    stringBuilder.append("D");
+                                    break;
+                                case 14:
+                                    stringBuilder.append("E");
+                                    break;
+                                case 15:
+                                    stringBuilder.append("F");
+                                    break;
+                            }
+                        } else
+                            stringBuilder.append(temp);
+                        M /= N;
+                    }
+                    stringBuilder.reverse();
+                    System.out.print("-");
+
+                    System.out.println(stringBuilder);
+                }
+
+            }
+
+        }
+
+
+    }
+
+
+    public static void main3(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            long count1 = in.nextInt();
+            long count2 = in.nextInt();
+            long count3 = in.nextInt();
+            long count4 = in.nextInt();
+            float A = (float) ((count1 + count3) / 2);
+            float B = (float) ((count2 + count4) / 2);
+            float C = (float) (count4 - B);
+            if ((A < 0 || B < 0 || C < 0)) System.out.println("No");
+            else System.out.println(A + " " + B + " " + C);
+        }
+    }
+
+
+    public static void main1(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
