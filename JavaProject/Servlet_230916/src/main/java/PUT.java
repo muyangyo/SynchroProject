32e1d5fdd55cf727b24358f7bda9a7b2b22f1845
沐阳 <3560775787@qq.com>
2023-10-08 23:14:07 +0800

    Update PUT.java

--- JavaProject/Servlet_230916/src/main/java/PUT.java
+++ JavaProject/Servlet_230916/src/main/java/PUT.java
@@ -18,8 +18,8 @@ public class PUT extends HttpServlet {
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //设置 "Access-Control-Allow-Origin" 响应头
         resp.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/");
-        resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF8");
+        resp.setContentType("text/html");
         resp.getWriter().println("你好!");
         resp.getWriter().flush();
     }
