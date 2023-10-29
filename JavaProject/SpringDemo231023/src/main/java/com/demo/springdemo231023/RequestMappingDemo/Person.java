9dd0b49cd5ac62636dcdd1f49bb8e68fffb13c03
沐阳 <3560775787@qq.com>
2023-10-29 21:09:56 +0800

    更新231029

--- JavaProject/SpringDemo231023/src/main/java/com/demo/springdemo231023/RequestMappingDemo/Person.java
+++ JavaProject/SpringDemo231023/src/main/java/com/demo/springdemo231023/RequestMappingDemo/Person.java
@@ -1,5 +1,9 @@
 package com.demo.springdemo231023.RequestMappingDemo;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 创建于 IntelliJ IDEA.
  * 描述：
@@ -8,30 +12,7 @@ package com.demo.springdemo231023.RequestMappingDemo;
  * Time: 17:59
  */
 public class Person {
-    public Integer id;
-    public String name;
-
-    @Override
-    public String toString() {
-        return "Person {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Getter @Setter
+    private Integer id;
+    private String name;
 }
