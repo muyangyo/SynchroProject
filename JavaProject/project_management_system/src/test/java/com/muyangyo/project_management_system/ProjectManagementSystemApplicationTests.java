72eb28bd48c3a712ab2e23a09db1b44293707c96
沐阳. <3560775787@qq.com>
2024-10-10 21:26:24 +0800

    更新241010

--- JavaProject/project_management_system/src/test/java/com/muyangyo/project_management_system/ProjectManagementSystemApplicationTests.java
+++ JavaProject/project_management_system/src/test/java/com/muyangyo/project_management_system/ProjectManagementSystemApplicationTests.java
@@ -1,5 +1,8 @@
 package com.muyangyo.project_management_system;
 
+import com.muyangyo.project_management_system.global.model.User;
+import com.muyangyo.project_management_system.global.model.request_model.ForLogin;
+import converter.MetaToVoConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +13,16 @@ class ProjectManagementSystemApplicationTests {
     void contextLoads() {
     }
 
+
+    @Test
+    void Test() throws Exception {
+        User user = new User();
+        user.setUsername("username");
+        user.setPassword("password");
+        user.setNickname("nickname");
+
+        ForLogin convert = MetaToVoConverter.convert(user, ForLogin.class);
+
+        System.out.println(convert);
+    }
 }
